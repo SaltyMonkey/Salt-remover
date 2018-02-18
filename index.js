ae86eebc8981059f68b4c4a192ab305865dcd8af
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2018-02-18 16:07:55 +0300

    Autoupdate support, command support

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+const sysmsg = require('tera-data-parser').sysmsg;
+Command = require('command');
+
 module.exports = function SaltRemover(dispatch) {
-    dispatch.hook('S_SYSTEM_MESSAGE', 1, event => {
-        if (event.message.includes("@2400")) {
-            return false
+    const command = Command(dispatch);
+    let enabled = true;
+
+    command.add('salt', (option) => {
+        switch (option) {
+            case 'on':
+                command.message('[Salt-Remover] Activated');
+                enabled = true;
+                break;
+            case 'off':
+                command.message('[Salt-Remover] Deactivated');
+                enabled = false;
+                break;
         }
+    });
+
+    dispatch.hook('S_SYSTEM_MESSAGE', 1, (event) => {
+        if (enabled)
+            event.message.startsWith(`@${sysmsg.maps.get(dispatch.base.protocolVersion).name.get('SMT_GACHA_REWARD')}\v`) ? false : undefined
     });
 };
